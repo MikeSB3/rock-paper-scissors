f32ed088d7e36d2e0acb07116b25b08e25fa34fc
Michael Berryman <berryman.michael@yahoo.com>
2020-12-11 17:28:27 -0800

    Allow a round to be played in the console

--- script.js
+++ script.js
@@ -1,5 +1,39 @@
-computerChoices = ["Rock", "Paper", "Scissors"];
+let computerChoices = ["Rock", "Paper", "Scissors"];
+const playerSelection = String(prompt("Choose Rock, Paper, or Scissors: ")).toLowerCase();
+const computerSelection = computerPlay().toLowerCase();
 
 function computerPlay() {
     return computerChoices[Math.floor(Math.random() * computerChoices.length)];
 }
+
+function playRound(player, computer) {
+    player = playerSelection;
+    computer = computerSelection;
+
+    if (player === computer) {
+        console.log("Tie game!");
+    }
+
+    if (player === "rock" && computer === "scissors") {
+        console.log("Player wins!");
+    } else if (player === "rock" && computer === "paper") {
+        console.log("Computer wins!");
+    }
+
+    if (player === "paper" && computer === "rock") {
+        console.log("Player wins!");
+    } else if (player === "paper" && computer === "scissors") {
+        console.log("Computer wins!");
+    }
+
+    if (player === "scissors" && computer === "paper") {
+        console.log("Player wins!");
+    } else if (player === "scissors" && computer === "rock") {
+        console.log("Computer wins!");
+    }
+}
+
+console.log("Player Choice: " + playerSelection);
+console.log("Computer Choice: " + computerSelection);
+
+playRound();
